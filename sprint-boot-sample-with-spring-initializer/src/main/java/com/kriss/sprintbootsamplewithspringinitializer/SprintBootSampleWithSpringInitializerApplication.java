c6ee7b9a440efaf4d9c2ad221f2aad2b5ff1ab9d
krishgo <krishgo@VWGOAAHL320692.ws.vwoa.na.vwg>
2019-03-29 15:51:16 -0400

    Initial commit

---
+++ sprint-boot-sample-with-spring-initializer/src/main/java/com/kriss/sprintbootsamplewithspringinitializer/SprintBootSampleWithSpringInitializerApplication.java
@@ -0,0 +1,13 @@
+package com.kriss.sprintbootsamplewithspringinitializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootSampleWithSpringInitializerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootSampleWithSpringInitializerApplication.class, args);
+	}
+
+}
